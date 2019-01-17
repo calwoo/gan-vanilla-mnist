e6cdd1cfc2257581ea859f280f3072108746c3bb
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-01-16 19:27:17 -0500

    learning about graphkeys

--- gan.py
+++ gan.py
@@ -10,16 +10,38 @@ x = tf.placeholder(tf.float32, [None, 784], name="inputs_x")
 z = tf.placeholder(tf.float32, [None, 100], name="inputs_z")
 
 def generator(z):
-    gen_fc1 = tf.layers.Dense(z, 128, tf.nn.relu)
-    gen_output = tf.layers.Dense(gen_fc1, 784, tf.nn.sigmoid)
+    with tf.variable_scope("generator"):
+        gen_fc1 = tf.layers.Dense(z, 128, tf.nn.relu)
+        gen_output = tf.layers.Dense(gen_fc1, 784, tf.nn.sigmoid)
     return gen_output
 
 def discriminator(x):
-    disc_fc1 = tf.layers.Dense(x, 128, tf.nn.relu)
-    disc_logit = tf.layers.Dense(disc_fc1, 1)
-    disc_prob = tf.nn.sigmoid(disc_logit)
+    with tf.variable_scope("discriminator"):
+        disc_fc1 = tf.layers.Dense(x, 128, tf.nn.relu)
+        disc_logit = tf.layers.Dense(disc_fc1, 1)
+        disc_prob = tf.nn.sigmoid(disc_logit)
     return disc_logit, disc_prob
 
 # the magical loss function
+noise_samples = generator(z)
 
+"""
+Our GAN discriminator will see a batch of real samples and fake (noise) samples and pump out the probabilities
+of them being real. We will then feed these probabilities into the loss function, which will be backpropagated
+through both generator/discriminator networks to train them.
+"""
+disc_real_logits, disc_real_prob = discriminator(x)
+disc_fake_logits, disc_fake_prob = discriminator(noise_samples)
+
+disc_loss = -tf.reduce_mean(tf.log(disc_real_prob) + tf.log(1-disc_fake_prob))
+gen_loss = tf.reduce_mean(tf.log(1-disc_fake_prob))
+
+disc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="discriminator")
+gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="generator")
+
+# gradient train
+disc_optimizer = tf.train.AdamOptimizer().minimize(disc_loss, var_list=disc_vars)
+
+for var in disc_vars:
+    print(var)
 
